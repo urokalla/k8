b12dd11a35f50a4a150500506527e77721bf9da8
urokalla <udairokalla@gmail.com>
2020-05-15 14:18:02 +0200

    go commit

---
+++ go/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("udai" + "Divya")
+	fmt.Println("add numbers:", 1+1)
+	fmt.Println("Division:", 8.0/2.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
